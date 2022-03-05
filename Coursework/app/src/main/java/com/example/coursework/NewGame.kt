78ed62ed1c0208021872152f9c4447d90b419bbf
AbdallahRuski <91491802+AbdallahRuski@users.noreply.github.com>
2022-03-05 11:17:51 +0530

    Button functionality

---
+++ Coursework/app/src/main/java/com/example/coursework/NewGame.kt
@@ -0,0 +1,21 @@
+package com.example.coursework
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class NewGame : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_game)
+
+
+
+    }
+}
